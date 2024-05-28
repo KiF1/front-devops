5ba3711c0ae0ccef7eafc266976466b8a0711848
abners <abners@cpqd.com.br>
2024-05-28 10:16:46 -0300

    new endpoint back

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '54.90.245.34', 'spacetime-devops.s3.amazonaws.com'],
+    domains: ['avatars.githubusercontent.com', '54.234.111.72', 'spacetime-devops.s3.amazonaws.com'],
   },
 }
 
