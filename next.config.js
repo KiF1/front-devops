6e4c4db0b6d80f30844c99fd11381fee258739ec
abner <abner.kif1@gmail.com>
2024-06-05 08:54:23 -0300

    add pipeline

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '54.234.111.72', 'spacetime-devops.s3.amazonaws.com'],
+    domains: ['avatars.githubusercontent.com', '54.196.243.201', 'spacetime-devops.s3.amazonaws.com'],
   },
 }
 
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://54.234.111.72:3333',
+  baseURL: 'http://54.196.243.201:3333',
 })
 
 export default api
